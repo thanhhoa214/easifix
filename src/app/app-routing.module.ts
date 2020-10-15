7bf4dca1a95440fe4135ffe26f162b6eb770acfa
tamntse13273 <72872088+tamntse13273@users.noreply.github.com>
2020-10-15 13:41:16 +0700

    Done Screen promotion-detail-screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PromotionDetailScreenComponent } from './promotion-detail-screen/promotion-detail-screen.component';
 import { SigninComponent } from './signin/signin.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'signin',
     component: SigninComponent,
+  },
+  {
+    path: 'promotion-detail-screen',
+    component: PromotionDetailScreenComponent,
   },
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SigninComponent } from './signin/signin.component';
+import { PromotionDetailScreenComponent } from './promotion-detail-screen/promotion-detail-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    SigninComponent
+    SigninComponent,
+    PromotionDetailScreenComponent
   ],
   imports: [
     BrowserModule,
