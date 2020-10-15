c0a730d65e3ff99f1fce6cec22c86e66b82f7845
tamntse13273 <72872088+tamntse13273@users.noreply.github.com>
2020-10-15 15:37:08 +0700

    Done Screen pending-after-request4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PendingAfterRequest4Component } from './pending-after-request4/pending-after-request4.component';
 import { SigninComponent } from './signin/signin.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'signin',
     component: SigninComponent,
+  },
+  {
+    path: 'pending-after-request4',
+    component: PendingAfterRequest4Component,
   },
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SigninComponent } from './signin/signin.component';
+import { PendingAfterRequest4Component } from './pending-after-request4/pending-after-request4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    SigninComponent
+    SigninComponent,
+    PendingAfterRequest4Component
   ],
   imports: [
     BrowserModule,
