812af64bde2baf944f2cf69d361791a5c1574004
tamntse13273 <72872088+tamntse13273@users.noreply.github.com>
2020-10-15 14:42:31 +0700

    Done Screen confirm-request-address

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConfirmRequestAddressComponent } from './confirm-request-address/confirm-request-address.component';
 import { SigninComponent } from './signin/signin.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'signin',
     component: SigninComponent,
+  },
+  {
+    path: 'confirm-request-address',
+    component: ConfirmRequestAddressComponent,
   },
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { SigninComponent } from './signin/signin.component';
+import { ConfirmRequestAddressComponent } from './confirm-request-address/confirm-request-address.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    SigninComponent
+    SigninComponent,
+    ConfirmRequestAddressComponent
   ],
   imports: [
     BrowserModule,
