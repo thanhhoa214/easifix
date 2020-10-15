2425b87798f24faf26acde17159584a1a36fbaf9
truongvh080999 <51153451+truongvh080999@users.noreply.github.com>
2020-10-15 10:21:42 +0700

    Done Screen history-complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HistoryCompleteComponent } from './history-complete/history-complete.component';
 import { SigninComponent } from './signin/signin.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'signin',
     component: SigninComponent,
+  },
+  {
+    path: 'history-complete',
+    component: HistoryCompleteComponent,
   },
   { path: '', pathMatch: 'full', redirectTo: 'welcome' },
 ];
